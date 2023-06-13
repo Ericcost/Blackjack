78e0b150d121c6672742a1fa7724af3ee9552845
Ericcost <eric.costerousse@gmail.com>
2023-06-13 23:29:38 +0200

    Add Play and check_winner methods to the Game class

--- BlackJack/blackjack.py
+++ BlackJack/blackjack.py
@@ -101,11 +101,49 @@ class Game:
         game_number = 0
         games_to_play = 0
 
-        try: 
-            game_to_play = int(input("How many games do you want to play ? "))
-        except:
-            
+        while games_to_play <= 0:
+            try: 
+                games_to_play = int(input("How many games do you want to play ? "))
+            except:
+                print('You must enter a number')
+        
+        while game_number < games_to_play:
+            game_number += 1
 
+            deck = Deck()
+            deck.shuffle()
+
+            player_hand = Hand()
+            dealer_hand = Hand(dealer=True)
+
+            for i in range(2): 
+                player_hand.add_card(deck.deal(1))
+                dealer_hand.add_card(deck.deal(1))
+
+            print()
+            print("*" * 30)
+            print(f"Game {game_number} of {games_to_play}")
+            print("*" * 30)
+            player_hand.display()
+            dealer_hand.display()
+
+    def check_winner(self, player_hand, dealer_hand, game_over=False):
+        if player_hand.get_value() > 21:
+            print("You busted. Dealer wins")
+            return True
+        elif dealer_hand.get_value() > 21:
+            print("Dealer busted. You win")
+            return True
+        elif dealer_hand.is_blackjack() and dealer_hand.is_blackjack():
+            print("Both players have blackjack. Tie !")
+            return True
+        elif dealer_hand.is_blackjack():
+            print("You have a blackjack. You win !")
+            return True
+        elif dealer_hand.is_blackjack():
+            print("Dealer has blackjack. Dealer wins !")
+            return True
+        return False
 
 g = Game()
 g.play()
