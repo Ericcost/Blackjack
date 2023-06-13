242fcaefa708ece8c6a386ee7bdcf6ca5698566f
Ericcost <eric.costerousse@gmail.com>
2023-06-13 18:03:13 +0200

    Try to understand how does a class in Python works

--- Exercices/test.py
+++ Exercices/test.py
@@ -18,3 +18,16 @@ years old
 """)
 
 print(name.islower())
+
+#Classes
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print("wouf")
+
+roger = Dog("Roger", 12)
+print(roger.name, roger.age)
+roger.bark()
