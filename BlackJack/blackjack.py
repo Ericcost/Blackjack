ccb9f88a36c04f0304b2e6fbc5001dbfe372b96f
Ericcost <eric.costerousse@gmail.com>
2023-06-13 23:03:40 +0200

    Add the calculate_value / get_value / is_blackjack / display  methods

--- BlackJack/blackjack.py
+++ BlackJack/blackjack.py
@@ -61,13 +61,41 @@ class Hand:
     def add_card(self, card_list):
         self.cards.extend(card_list)
 
+    def calculate_value(self):
+        self.value = 0 
+        has_ace = False
+
+        for card in self.cards:
+            card_value = int(card.rank['value'])
+            self.value += card_value
+            if card.rank['value'] == "A":
+                has_ace = True
+                
+        if has_ace and self.value > 21:
+            self.value -= 10
+        # return self.value
+    
+    def get_value(self):
+        self.calculate_value()
+        return self.value
+    
+    def  is_blackjack(self):
+        return self.get_value() == 21
+    
+    def display(self):
+        print(f''' {"Dealer's" if self.dealer else "Your"} hand:  ''')
+        for card in self.cards:
+            print(card)
+
+        if not self.dealer:
+            print(f"Value: {self.get_value()}")
+        
+
+
 deck = Deck()
 deck.shuffle()
 
 hand = Hand()
 hand.add_card(deck.deal(2))
 
-for card in hand.cards:
-    print(card)
-
-print(len(deck.cards))
+hand.display()
